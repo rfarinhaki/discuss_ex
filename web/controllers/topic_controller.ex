88e627da23653becd6a624f2a95c47d742bd4fa3
Ricardo Farinhaki <ricardofarinhaki@gmail.com>
2020-10-20 17:51:44 -0300

    added POST method for the form submission

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,9 +1,25 @@
 defmodule Discuss.TopicController do 
   use Discuss.Web, :controller
+  
+  alias Discuss.Topic
 
-  def new(conn, _params) do 
-    IO.puts "++++"
-    IO.inspect conn
-    IO.puts "++++"
+  def new(conn, params) do 
+    struct = %Topic{}
+
+    params = %{}
+
+    changeset = Topic.changeset(struct, params)
+
+    render conn, "new.html", changeset: changeset
   end
+
+  def create(conn, params) do
+    #IO.puts("+++++")
+    #IO.inspect(params)
+    #IO.puts("+++++")
+
+    %{"topic" => topic} = params
+    IO.inspect(topic)
+  end
+  
 end
