8c24e87cdbec5ae3bf83cbdeeccfdd7ad3c032e9
Ricardo Farinhaki <ricardofarinhaki@gmail.com>
2020-10-20 20:02:24 -0300

    Changed the index.html to the list of topics and redirecting from new when the creation is successful

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,6 +3,13 @@ defmodule Discuss.TopicController do
   
   alias Discuss.Topic
 
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, params) do 
     struct = %Topic{}
 
@@ -20,7 +27,10 @@ defmodule Discuss.TopicController do
 
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, post} -> 
+        conn 
+        |> put_flash(:info, "Topic created")
+        |> redirect(to: topic_path(conn, :index))
       {:error, changeset} -> 
         render conn, "new.html", changeset: changeset
     end
