52ec5631f2aa6fc5e7409852b36cbe38e7d906d0
Ricardo Farinhaki <ricardofarinhaki@gmail.com>
2020-10-21 22:08:04 -0300

    Added github OAuth to provide session and login support

--- web/router.ex
+++ web/router.ex
@@ -22,6 +22,15 @@ defmodule Discuss.Router do
     get "/topics/:id/edit", TopicController, :edit
     put "/topics/:id", TopicController, :update
     delete "/topics/:id", TopicController, :delete
+    #ou
+    #resources "/", TopicController
+  end
+
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :request #defined by ueberauth
+    get "/:provider/callback", AuthController, :callback
   end
 
   # Other scopes may use custom stacks.
